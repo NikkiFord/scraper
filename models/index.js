e906b804b713b39e96bc5ca41149bcb6436e439a
Nikki Ford <nikkigford@gmail.com>
2019-12-17 21:47:56 -0700

    changed mongo code for heroku

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://nikki:Central6@fit-to-scrape-pfok0.azure.mongodb.net/test?retryWrites=true&w=majority");
+mongoose.connect("mongodb://nikki:F5d9117FKHvj@ds064188.mlab.com:64188/scraper-home-work");
 
 module.exports = {
   User: require("./user.js"),
